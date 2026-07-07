68755a46bd1f3a546badafab77f775d0894d13ac
yannickcruz <yannickcruz02@gmail.com>
2026-07-07 19:30:33 -0300

    fix: Adicionado cláusula para gerar herança com tabelas individuais no BD

--- src/modelo/Pessoa.java
+++ src/modelo/Pessoa.java
@@ -4,8 +4,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 
 @Entity
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public class Pessoa {
 
 	@Id @GeneratedValue
